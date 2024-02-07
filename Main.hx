3027f6989253f883def95ae0eadda2d3e1630435
Gidk-g <devhaxel@gmail.com>
2024-02-07 19:06:21 +0100

    fix Sprite

--- Main.hx
+++ Main.hx
@@ -8,7 +8,9 @@ class Main extends Flexo {
 
     override function render() {
         var sprite:Sprite = new Sprite();
-        sprite.loadImage("assets/robot.png", 32, 32);
+        sprite.loadImage("assets/robot.png", 32, 32, true);
+        sprite.addAnimation("sigma", [0, 1, 2, 3]);
+        sprite.playAnimation("sigma", true);
         sprite.setPosition(100, 100);
         sprite.setScale(2.0, 2.0);
         addSprite(sprite);
@@ -17,9 +19,6 @@ class Main extends Flexo {
     }
 
     override function handleEvents() {
-        if (flexo.Keyboard.isKeyDown(SPACE)) {
-            trace("Spacja jest wciśnięta!");
-        }
         super.handleEvents();
     }
 
--- flexo/Sprite.hx
+++ flexo/Sprite.hx
@@ -11,29 +11,37 @@ class Sprite {
     public var textureSize:Point;
     public var position:Point;
     public var scale:FPoint;
+    public var animations:Map<String, Array<Int>>;
+    public var currentAnimation:String;
+    public var currentAnimationFrame:Int;
+    public var isAnimationPlaying:Bool;
+    public var loopAnimation:Bool;
+    public var animationTimer:Timer;
     public var frameWidth:Int;
     public var frameHeight:Int;
-    private var animations:Map<String, Array<Int>>;
-    private var currentAnimation:String;
-    private var animationFrames:Array<Int>;
-    private var animationIndex:Int;
-    private var isAnimating:Bool;
-    private var isLooping:Bool;
+    public var isSpriteSheet:Bool;
 
     public function new() {
         position = Point.create(0, 0);
         scale = FPoint.create(1, 1);
         animations = new Map<String, Array<Int>>();
         currentAnimation = "";
-        animationIndex = 0;
-        isAnimating = false;
-        isLooping = false;
+        currentAnimationFrame = 0;
+        isAnimationPlaying = false;
+        loopAnimation = false;
+        isSpriteSheet = false;
     }
 
-    public function loadImage(path:String, frameWidth:Int = 0, frameHeight:Int = 0):Void {
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
+    public function loadImage(path:String, frameWidth:Int = 0, frameHeight:Int = 0, isSpriteSheet:Bool = false):Void {
+        this.isSpriteSheet = isSpriteSheet;
+        if (isSpriteSheet) {
+            loadSpriteSheet(path, frameWidth, frameHeight);
+        } else {
+            loadSingleImage(path);
+        }
+    }
 
+    private function loadSingleImage(path:String):Void {
         var surface:Surface = Image.load(path);
         if (surface == null) {
             Sys.println("Failed to load image: " + path);
@@ -48,29 +56,91 @@ class Sprite {
 
         textureSize = SDL.getTextureSize(texture);
         SDL.freeSurface(surface);
+
+        this.frameWidth = textureSize.x;
+        this.frameHeight = textureSize.y;
+    }
+
+    private function loadSpriteSheet(path:String, frameWidth:Int, frameHeight:Int):Void {
+        var surface:Surface = Image.load(path);
+        if (surface == null) {
+            Sys.println("Failed to load image: " + path);
+            return;
+        }
+
+        texture = SDL.createTextureFromSurface(Flexo.renderer, surface);
+        if (texture == null) {
+            Sys.println("Failed to create texture from image: " + path);
+            return;
+        }
+
+        textureSize = SDL.getTextureSize(texture);
+        SDL.freeSurface(surface);
+
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+
+        var framesPerRow:Int = Std.int(textureSize.x / frameWidth);
+        var framesPerColumn:Int = Std.int(textureSize.y / frameHeight);
+        for (i in 0...framesPerRow) {
+            for (j in 0...framesPerColumn) {
+                var frameIndex:Int = j * framesPerRow + i;
+                var frameName:String = "Frame_" + frameIndex;
+                animations.set(frameName, [frameIndex]);
+            }
+        }
+    }
+
+    public function addAnimation(name:String, frames:Array<Int>):Void {
+        animations.set(name, frames);
+    }
+
+    public function playAnimation(name:String, loop:Bool = false):Void {
+        currentAnimation = name;
+        currentAnimationFrame = 0;
+        isAnimationPlaying = true;
+        loopAnimation = loop;
+        animationTimer = new Timer(Std.int(1000 / 60));
+        animationTimer.run = advanceAnimationFrame;
+    }
+
+    public function stopAnimation():Void {
+        isAnimationPlaying = false;
+        animationTimer.stop();
+    }
+
+    private function advanceAnimationFrame():Void {
+        var frames:Array<Int> = animations.get(currentAnimation);
+        if (currentAnimationFrame < frames.length - 1) {
+            currentAnimationFrame++;
+        } else if (loopAnimation) {
+            currentAnimationFrame = 0;
+        } else {
+            stopAnimation();
+        }
     }
 
     public function render(?targetX:Int = null, ?targetY:Int = null, ?targetWidth:Int = null, ?targetHeight:Int = null) {
-        if (isAnimating) {
-            var currentFrame = animationFrames[animationIndex];
-            var sourceRect = Rectangle.create(currentFrame * frameWidth, 0, frameWidth, frameHeight);
-            var destRect = Rectangle.create(
+        var srcRect:Rectangle;
+
+        if (isSpriteSheet) {
+            var frameIndex:Int = animations.get(currentAnimation)[currentAnimationFrame];
+            var frameColumn:Int = Std.int(frameIndex % (textureSize.x / frameWidth));
+            var frameRow:Int = Std.int(frameIndex / (textureSize.y / frameWidth));
+            srcRect = Rectangle.create(frameWidth * frameColumn, frameHeight * frameRow, frameWidth, frameHeight);
+        } else {
+            srcRect = Rectangle.create(0, 0, textureSize.x, textureSize.y);
+        }
+
+        SDL.renderCopy(Flexo.renderer, texture,
+            srcRect,
+            Rectangle.create(
                 targetX != null ? targetX : position.x,
                 targetY != null ? targetY : position.y,
                 targetWidth != null ? targetWidth : Std.int(frameWidth * scale.x),
                 targetHeight != null ? targetHeight : Std.int(frameHeight * scale.y)
-            );
-            SDL.renderCopy(Flexo.renderer, texture, sourceRect, destRect);
-        } else {
-            var sourceRect = Rectangle.create(0, 0, textureSize.x, textureSize.y);
-            var destRect = Rectangle.create(
-                targetX != null ? targetX : position.x,
-                targetY != null ? targetY : position.y,
-                targetWidth != null ? targetWidth : Std.int(textureSize.x * scale.x),
-                targetHeight != null ? targetHeight : Std.int(textureSize.y * scale.y)
-            );
-            SDL.renderCopy(Flexo.renderer, texture, sourceRect, destRect);
-        }
+            )
+        );
     }
 
     public function setPosition(x:Int, y:Int):Void {
@@ -81,42 +151,6 @@ class Sprite {
     public function setScale(scaleX:Float, scaleY:Float):Void {
         scale.x = scaleX;
         scale.y = scaleY;
-    }
-
-    public function addAnimation(name:String, frames:Array<Int>):Void {
-        animations.set(name, frames);
-    }
-
-    public function playAnimation(name:String, loop:Bool = false):Void {
-        if (!animations.exists(name)) {
-            Sys.println("Animation " + name + " not found.");
-            return;
-        }
-
-        currentAnimation = name;
-        animationFrames = animations.get(name);
-        animationIndex = 0;
-        isAnimating = true;
-        isLooping = loop;
-
-        if (isAnimating) {
-            Timer.delay(playNextFrame, Std.int(1000 / 12));
-        }
-    }
-
-    private function playNextFrame():Void {
-        if (isAnimating) {
-            animationIndex++;
-            if (animationIndex >= animationFrames.length) {
-                if (isLooping) {
-                    animationIndex = 0;
-                } else {
-                    isAnimating = false;
-                    return;
-                }
-            }
-            Timer.delay(playNextFrame, Std.int(1000 / 12));
-        }
     }
 
     public static function cleanup() {
